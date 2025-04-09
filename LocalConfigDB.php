0f8a2d280576d2ccee3692c94e196499d928ab71
Dat Tran <datforitwork@gmail.com>
2025-04-09 14:25:01 +0700

    test xem chay duoc khogn

--- LocalConfigDB.php
+++ LocalConfigDB.php
@@ -4,7 +4,7 @@ return [
     'host'      => 'localhost',
     'database'  => 'doantotnghiep',
     'username'  => 'root',
-    'password'  => '',
+    'password'  => '123456',
     'charset'   => 'utf8',
     'options'   => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
